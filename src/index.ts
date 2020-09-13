1d7c20e076af72f3b2e78af9cdb434c52ee07815
Adam Kadlec <adam.kadlec@gmail.com>
2020-09-13 11:49:08 +0200

    Transforming

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,63 @@
+import { VueConstructor } from 'vue'
+
 // Forms components
+// @ts-ignore
 import FbFormInput from './components/Forms/FbInput'
+// @ts-ignore
 import FbFormSelect from './components/Forms/FbSelect'
+// @ts-ignore
 import FbFormTextArea from './components/Forms/FbTextArea'
+// @ts-ignore
 import FbFormCheckboxesGroup from './components/Forms/FbCheckboxesGroup'
+// @ts-ignore
 import FbFormRadioButton from './components/Forms/FbRadioButton'
+// @ts-ignore
 import FbFormRadioButtonsGroup from './components/Forms/FbRadioButtonsGroup'
 
 // MD forms components
+// @ts-ignore
 import FbFormCheckbox from './components/Forms/FbCheckbox'
+// @ts-ignore
 import FbMdFormDatePicker from './components/Forms/FbMdDatePicker'
+// @ts-ignore
 import FbMdFormInput from './components/Forms/FbMdInput'
+// @ts-ignore
 import FbMdFormSelect from './components/Forms/FbMdSelect'
+// @ts-ignore
 import FbMdFormTextArea from './components/Forms/FbMdTextArea'
 
 // UI components
+// @ts-ignore
 import FbAlert from './components/UI/FbAlert'
+// @ts-ignore
 import FbButton from './components/UI/FbButton'
+// @ts-ignore
 import FbCardBox from './components/UI/FbCardBox'
+// @ts-ignore
 import FbConfirmationWindow from './components/UI/FbConfirmationWindow'
+// @ts-ignore
 import FbContent from './components/UI/FbContent'
+// @ts-ignore
 import FbModalForm from './components/UI/FbModalForm'
+// @ts-ignore
 import FbModalInfo from './components/UI/FbModalInfo'
+// @ts-ignore
 import FbModalWindow from './components/UI/FbModalWindow'
+// @ts-ignore
 import FbDivider from './components/UI/FbDivider'
+// @ts-ignore
 import FbSwitchElement from './components/UI/FbSwitchElement'
+// @ts-ignore
 import FbLoadingBox from './components/UI/FbLoadingBox'
+// @ts-ignore
 import FbPageLoading from './components/UI/FbPageLoading'
+// @ts-ignore
 import FbResultErr from './components/UI/FbResultErr'
+// @ts-ignore
 import FbResultOk from './components/UI/FbResultOk'
+// @ts-ignore
 import FbSpinner from './components/UI/FbSpinner'
+// @ts-ignore
 import FbTransitionExpand from './components/UI/FbTransitionExpand'
 
 // Mixins
@@ -38,9 +67,12 @@ import ThemeHelpersMixin from './mixins/helpers'
 import ThemeClickOutsideDirective from './directives/ClickOutside'
 import ThemeBodyScrollLockDirective from './directives/BodyScrollLock'
 
-export default {
+const Plugin = {
+  installed: false,
 
-  install(Vue) {
+  install(
+      Vue: VueConstructor,
+  ) {
     // Classic design
     Vue.component('FbFormInput', FbFormInput)
     Vue.component('FbFormSelect', FbFormSelect)
@@ -82,5 +114,11 @@ export default {
     Vue.directive('clickOutside', ThemeClickOutsideDirective)
     Vue.directive('bodyScrollLock', ThemeBodyScrollLockDirective)
   },
-
 }
+
+// Auto install
+if (typeof window !== 'undefined' && Object.prototype.hasOwnProperty.call(window, 'Vue')) {
+  (window as any).Vue.use(Plugin.install)
+}
+
+export default Plugin
