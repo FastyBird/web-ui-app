cdbeedb7658e8e406ef6eb7f8df37c49f7f1c951
Adam Kadlec <adam.kadlec@gmail.com>
2022-03-07 19:39:17 +0100

    Drop layout heeader buttons spacer (#22)

--- src/components/layout/FbHeader/Button/types.ts
+++ src/components/layout/FbHeader/Button/types.ts
@@ -3,7 +3,6 @@ import { FbMenuItemTypes } from "@/types";
 export interface IFbLayoutHeaderButtonProps {
   actionType: FbMenuItemTypes;
   action: string | { [key: string]: any } | null;
-  label: string | null;
   small: boolean;
   left: boolean;
   right: boolean;
--- src/components/layout/FbHeader/index.ts
+++ src/components/layout/FbHeader/index.ts
@@ -3,4 +3,3 @@ export { default as FbLayoutHeaderButton } from "./Button/index.vue";
 export { default as FbLayoutHeaderContent } from "./Content/index.vue";
 export { default as FbLayoutHeaderHeading } from "./Heading/index.vue";
 export { default as FbLayoutHeaderIcon } from "./Icon/index.vue";
-export { default as FbLayoutHeaderSpacer } from "./Spacer/index.vue";
