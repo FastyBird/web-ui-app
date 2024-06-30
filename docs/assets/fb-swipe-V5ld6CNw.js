6aebea0a4dd8dc583ce24f65581f85a5932a3a4f
GitHub <action@github.com>
2024-06-30 20:29:28 +0000

    [general] Application contaner (#261)

--- docs/assets/fb-swipe-V5ld6CNw.js
+++
@@ -1,7 +0,0 @@
-import{j as t}from"./jsx-runtime-Ch61IVPz.js";import{u as n}from"./index-y0NlMPzl.js";import{M as s,C as e}from"./index-DlXXc_Ve.js";import{S as a,B as m}from"./fb-swipe.stories-DC7t1qVZ.js";import"./iframe-BTIADPZA.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-DXimoRZY.js";import"./index-Dv1YWgiV.js";import"./index-DrFu-skq.js";import"./index-D36ykrkJ.js";import"./vue.esm-bundler-BcBoVmeq.js";import"./web-ui-components.es-kpXR5N9H.js";function r(o){const i={h1:"h1",h2:"h2",hr:"hr",...n(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:a}),`
-`,t.jsx(i.h1,{id:"swipe",children:"Swipe"}),`
-`,t.jsx(i.h2,{id:"basic-usage",children:"Basic Usage"}),`
-`,t.jsx(e,{withToolbar:!0,of:m}),`
-`,t.jsx(i.hr,{}),`
-`,t.jsx(i.h2,{id:"swipe-api",children:"Swipe API"}),`
-`,t.jsx("div",{style:{marginTop:"30px"}})]})}function S(o={}){const{wrapper:i}={...n(),...o.components};return i?t.jsx(i,{...o,children:t.jsx(r,{...o})}):r(o)}export{S as default};
