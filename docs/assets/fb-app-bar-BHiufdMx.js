db8e3a96313a7dd2618c23bfc65d6c366ecaf54a
GitHub <action@github.com>
2024-08-03 22:15:53 +0000

    Fixing QA for packages (#278)

--- docs/assets/fb-app-bar-BHiufdMx.js
+++
@@ -1,7 +0,0 @@
-import{j as r}from"./jsx-runtime-Dkckj8Dc.js";import{u as a}from"./index-Bl0HHB_P.js";import{ae as s,af as i}from"./index-Buto0diF.js";import{A as p,B as e}from"./fb-app-bar.stories-GDWHjnUA.js";import"./iframe-CTTwUqPA.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-DlJr0YTq.js";import"./index-DrFu-skq.js";import"./index-Cit3b8oi.js";import"./vue.esm-bundler-CCHDJWTD.js";import"./web-ui-components.es-CHpbMkEl.js";function o(n){const t={h1:"h1",h2:"h2",hr:"hr",...a(),...n.components};return r.jsxs(r.Fragment,{children:[r.jsx(s,{of:p}),`
-`,r.jsx(t.h1,{id:"app-bar",children:"App Bar"}),`
-`,r.jsx(t.h2,{id:"basic-usage",children:"Basic Usage"}),`
-`,r.jsx(i,{withAppBar:!0,of:e}),`
-`,r.jsx(t.hr,{}),`
-`,r.jsx(t.h2,{id:"app-bar-api",children:"App Bar API"}),`
-`,r.jsx("div",{style:{marginTop:"30px"}})]})}function C(n={}){const{wrapper:t}={...a(),...n.components};return t?r.jsx(t,{...n,children:r.jsx(o,{...n})}):o(n)}export{C as default};
--- docs/assets/intro-CUkHa_8e.js
+++
@@ -1,9 +0,0 @@
-import{j as r}from"./jsx-runtime-Dkckj8Dc.js";import{u as e}from"./index-Bl0HHB_P.js";import{ae as o}from"./index-Buto0diF.js";import"./iframe-CTTwUqPA.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-DlJr0YTq.js";import"./index-DrFu-skq.js";function n(i){const t={code:"code",h1:"h1",h4:"h4",p:"p",pre:"pre",...e(),...i.components};return r.jsxs(r.Fragment,{children:[r.jsx(o,{title:"About/Intro"}),`
-`,r.jsx(t.h1,{id:"fastybird-web-ui-library",children:"FastyBird Web UI library"}),`
-`,r.jsx(t.p,{children:"FastyBird Web UI library is a design system built with Storybook and Vue."}),`
-`,r.jsx(t.h4,{id:"installation",children:"Installation"}),`
-`,r.jsx(t.pre,{children:r.jsx(t.code,{children:`yarn add @fastybird/web-ui-library
-`})}),`
-`,r.jsx(t.p,{children:"Import the FastyBird UI main style sheet in your application:"}),`
-`,r.jsx(t.pre,{children:r.jsx(t.code,{children:`@import "~@fastybird/web-ui-library/web-ui-library.css";
-`})})]})}function y(i={}){const{wrapper:t}={...e(),...i.components};return t?r.jsx(t,{...i,children:r.jsx(n,{...i})}):n(i)}export{y as default};
