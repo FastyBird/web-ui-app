2959f102f4df9cee78f94c893fe8764ff3554a62
akadlec <adam.kadlec@gmail.com>
2024-08-04 08:30:13 +0000

    Deploying to main from  @ 9b1cc512a24ff3bf27daeb0023cdb84209a3ab8d 🚀

---
+++ docs/assets/fb-bottom-navigation-DWgP2mfK.js
@@ -0,0 +1,7 @@
+import{j as t}from"./jsx-runtime-Bj0KVekq.js";import{u as a}from"./index-CgzWCmjo.js";import{ae as r,af as s}from"./index-BzRX9e_J.js";import{B as m,a as e}from"./fb-bottom-navigation.stories-54d_yei4.js";import"./iframe-CnzFZemG.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-DlJr0YTq.js";import"./index-DrFu-skq.js";import"./chunk-454WOBUV-CM0pFb8Z.js";import"./v4-CQkTLCs1.js";import"./web-ui-components.es-f1tHOnHV.js";import"./vue.esm-bundler-CCHDJWTD.js";function n(i){const o={h1:"h1",h2:"h2",hr:"hr",...a(),...i.components};return t.jsxs(t.Fragment,{children:[t.jsx(r,{of:m}),`
+`,t.jsx(o.h1,{id:"bottom-navigation",children:"Bottom Navigation"}),`
+`,t.jsx(o.h2,{id:"basic-usage",children:"Basic Usage"}),`
+`,t.jsx(s,{withBottomNavigation:!0,of:e}),`
+`,t.jsx(o.hr,{}),`
+`,t.jsx(o.h2,{id:"bottom-navigation-api",children:"Bottom Navigation API"}),`
+`,t.jsx("div",{style:{marginTop:"30px"}})]})}function N(i={}){const{wrapper:o}={...a(),...i.components};return o?t.jsx(o,{...i,children:t.jsx(n,{...i})}):n(i)}export{N as default};
