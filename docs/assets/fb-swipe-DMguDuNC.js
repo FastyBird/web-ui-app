742d2c0dbfcda763d1292dfe1398d4834be0f49a
akadlec <adam.kadlec@gmail.com>
2024-08-17 14:49:33 +0000

    Deploying to main from  @ 4798ec59236689134222e7bf9cf77a335fb049f5 🚀

---
+++ docs/assets/fb-swipe-DMguDuNC.js
@@ -0,0 +1,7 @@
+import{j as t}from"./jsx-runtime-BnRk6VGf.js";import{u as n}from"./index-CuZtdE_5.js";import{ae as s,af as e}from"./index-BS43z9uY.js";import{S as a,B as m}from"./fb-swipe.stories-BgjC3pmq.js";import"./iframe-bPwVLXY5.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-DlJr0YTq.js";import"./index-DrFu-skq.js";import"./index-CGSOqp07.js";import"./vue.esm-bundler-BaOJGPZw.js";import"./web-ui-components.es-DACsCjAi.js";function r(o){const i={h1:"h1",h2:"h2",hr:"hr",...n(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:a}),`
+`,t.jsx(i.h1,{id:"swipe",children:"Swipe"}),`
+`,t.jsx(i.h2,{id:"basic-usage",children:"Basic Usage"}),`
+`,t.jsx(e,{withToolbar:!0,of:m}),`
+`,t.jsx(i.hr,{}),`
+`,t.jsx(i.h2,{id:"swipe-api",children:"Swipe API"}),`
+`,t.jsx("div",{style:{marginTop:"30px"}})]})}function C(o={}){const{wrapper:i}={...n(),...o.components};return i?t.jsx(i,{...o,children:t.jsx(r,{...o})}):r(o)}export{C as default};
