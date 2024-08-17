a8588ede21b000845a76e1c668ab07354afa2511
akadlec <adam.kadlec@gmail.com>
2024-08-17 21:21:00 +0000

    Deploying to main from  @ 30f50b91da8abc333d6c4237201b4248e7758ba4 🚀

---
+++ docs/assets/fb-app-bar-CK5J2acM.js
@@ -0,0 +1,7 @@
+import{j as r}from"./jsx-runtime-BnRk6VGf.js";import{u as a}from"./index-CuZtdE_5.js";import{ae as s,af as i}from"./index-BS43z9uY.js";import{A as p,B as e}from"./fb-app-bar.stories-BvtecKFP.js";import"./iframe-bPwVLXY5.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-DlJr0YTq.js";import"./index-DrFu-skq.js";import"./index-CGSOqp07.js";import"./vue.esm-bundler-BaOJGPZw.js";import"./web-ui-components.es-DACsCjAi.js";function o(n){const t={h1:"h1",h2:"h2",hr:"hr",...a(),...n.components};return r.jsxs(r.Fragment,{children:[r.jsx(s,{of:p}),`
+`,r.jsx(t.h1,{id:"app-bar",children:"App Bar"}),`
+`,r.jsx(t.h2,{id:"basic-usage",children:"Basic Usage"}),`
+`,r.jsx(i,{withAppBar:!0,of:e}),`
+`,r.jsx(t.hr,{}),`
+`,r.jsx(t.h2,{id:"app-bar-api",children:"App Bar API"}),`
+`,r.jsx("div",{style:{marginTop:"30px"}})]})}function C(n={}){const{wrapper:t}={...a(),...n.components};return t?r.jsx(t,{...n,children:r.jsx(o,{...n})}):o(n)}export{C as default};
