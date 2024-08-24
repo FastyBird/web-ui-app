040509fb2c202aa589fd29b434255185c6ef95e6
akadlec <adam.kadlec@gmail.com>
2024-08-24 08:48:25 +0000

    Deploying to main from  @ c9d995d8536163bc648730af8adb91c2b801f441 🚀

---
+++ docs/assets/colors.stories-DazR1O9p.js
@@ -0,0 +1,29 @@
+const l={title:"About/Colors"},s=()=>({template:`
+      <div class="fb-story-colors__container">
+        <div class="fb-story-colors__colors">
+          <div class="fb-story-colors__colors-item fb-story-colors__colors-item-primary"><span class="fb-story-colors__colors-item-desc">primary</span></div>
+          <div class="fb-story-colors__colors-item fb-story-colors__colors-item-success"><span class="fb-story-colors__colors-item-desc">success</span></div>
+          <div class="fb-story-colors__colors-item fb-story-colors__colors-item-info"><span class="fb-story-colors__colors-item-desc">info</span></div>
+          <div class="fb-story-colors__colors-item fb-story-colors__colors-item-warning"><span class="fb-story-colors__colors-item-desc">warning</span></div>
+          <div class="fb-story-colors__colors-item fb-story-colors__colors-item-danger"><span class="fb-story-colors__colors-item-desc">danger</span></div>
+          <div class="fb-story-colors__colors-item fb-story-colors__colors-item-error"><span class="fb-story-colors__colors-item-desc">error</span></div>
+          <div class="fb-story-colors__colors-item fb-story-colors__colors-item-default"><span class="fb-story-colors__colors-item-desc">default</span></div>
+        </div>
+      </div>
+    `});var o,r,c;s.parameters={...s.parameters,docs:{...(o=s.parameters)==null?void 0:o.docs,source:{originalSource:`() => {
+  return {
+    template: \`
+      <div class="fb-story-colors__container">
+        <div class="fb-story-colors__colors">
+          <div class="fb-story-colors__colors-item fb-story-colors__colors-item-primary"><span class="fb-story-colors__colors-item-desc">primary</span></div>
+          <div class="fb-story-colors__colors-item fb-story-colors__colors-item-success"><span class="fb-story-colors__colors-item-desc">success</span></div>
+          <div class="fb-story-colors__colors-item fb-story-colors__colors-item-info"><span class="fb-story-colors__colors-item-desc">info</span></div>
+          <div class="fb-story-colors__colors-item fb-story-colors__colors-item-warning"><span class="fb-story-colors__colors-item-desc">warning</span></div>
+          <div class="fb-story-colors__colors-item fb-story-colors__colors-item-danger"><span class="fb-story-colors__colors-item-desc">danger</span></div>
+          <div class="fb-story-colors__colors-item fb-story-colors__colors-item-error"><span class="fb-story-colors__colors-item-desc">error</span></div>
+          <div class="fb-story-colors__colors-item fb-story-colors__colors-item-default"><span class="fb-story-colors__colors-item-desc">default</span></div>
+        </div>
+      </div>
+    \`
+  };
+}`,...(c=(r=s.parameters)==null?void 0:r.docs)==null?void 0:c.source}}};const t=["Colors"];export{s as Colors,t as __namedExportsOrder,l as default};
