e697c042b29daac738834f2a227ea5a43a4f982b
GitHub <action@github.com>
2024-08-27 11:45:59 +0000

    [virtual-thermostat-addon] Update temperature ranges (#304)

--- docs/assets/fb-swipe-CAx_mf3P.js
+++
@@ -1,7 +0,0 @@
-import{j as t}from"./jsx-runtime-DXCSIy6i.js";import{u as n}from"./index-D_KCP6KM.js";import{ae as s,af as e}from"./index-DQ7l6RrC.js";import{S as a,B as m}from"./fb-swipe.stories-DPmdXf-n.js";import"./iframe-C36Hli1d.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-DlJr0YTq.js";import"./index-DrFu-skq.js";import"./index-DRDbwcJ4.js";import"./vue.esm-bundler-BaOJGPZw.js";import"./web-ui-components.es-DQiNXqWY.js";function r(o){const i={h1:"h1",h2:"h2",hr:"hr",...n(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:a}),`
-`,t.jsx(i.h1,{id:"swipe",children:"Swipe"}),`
-`,t.jsx(i.h2,{id:"basic-usage",children:"Basic Usage"}),`
-`,t.jsx(e,{withToolbar:!0,of:m}),`
-`,t.jsx(i.hr,{}),`
-`,t.jsx(i.h2,{id:"swipe-api",children:"Swipe API"}),`
-`,t.jsx("div",{style:{marginTop:"30px"}})]})}function C(o={}){const{wrapper:i}={...n(),...o.components};return i?t.jsx(i,{...o,children:t.jsx(r,{...o})}):r(o)}export{C as default};
