581295348601cc4e669be61b7545e30c31765f01
akadlec <adam.kadlec@gmail.com>
2024-09-30 15:12:40 +0000

    Deploying to main from  @ 51221d8319264822ab7f289e7e0e59910cad6173 🚀

---
+++ docs/assets/fb-list-O9w7AagO.js
@@ -0,0 +1,9 @@
+import{ae as t,af as e,ag as o}from"./index-C6jGGYnR.js";import{u as n}from"./index-DIr4N83_.js";import{L as a,B as m,a as h}from"./fb-list.stories-B09SYjWv.js";import"./iframe-CAUctf-m.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-CxISiSMm.js";import"./index-DrFu-skq.js";import"./vue.esm-bundler-DY6RS4Md.js";import"./chunk-D5ZWXAHU-CGElDDNX.js";import"./v4-CQkTLCs1.js";import"./index-CPeZypXz.js";import"./web-ui-components.es-GVlHIQdK.js";function r(i){const s={h1:"h1",h2:"h2",hr:"hr",...n(),...i.components};return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:a}),`
+`,t.jsx(s.h1,{id:"list",children:"List"}),`
+`,t.jsx(s.h2,{id:"basic-usage",children:"Basic Usage"}),`
+`,t.jsx(o,{withToolbar:!0,of:m}),`
+`,t.jsx(s.h2,{id:"list-style",children:"List Style"}),`
+`,t.jsx(o,{withToolbar:!0,of:h}),`
+`,t.jsx(s.hr,{}),`
+`,t.jsx(s.h2,{id:"list-api",children:"List API"}),`
+`,t.jsx("div",{style:{marginTop:"30px"}})]})}function w(i={}){const{wrapper:s}={...n(),...i.components};return s?t.jsx(s,{...i,children:t.jsx(r,{...i})}):r(i)}export{w as default};
---
+++ docs/assets/fb-spinner-COstu4tr.js
@@ -0,0 +1,16 @@
+import{ae as e,af as c,ag as r,ah as o}from"./index-C6jGGYnR.js";import{u as t}from"./index-DIr4N83_.js";import{S as h,C as i,a as l,V as a}from"./fb-spinner.stories-BgkX9ni_.js";import"./iframe-CAUctf-m.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-CxISiSMm.js";import"./index-DrFu-skq.js";import"./web-ui-components.es-GVlHIQdK.js";import"./vue.esm-bundler-DY6RS4Md.js";function d(s){const n={code:"code",h1:"h1",h2:"h2",h3:"h3",hr:"hr",p:"p",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",...t(),...s.components};return e.jsxs(e.Fragment,{children:[e.jsx(c,{of:h}),`
+`,e.jsx(n.h1,{id:"spinner",children:"Spinner"}),`
+`,e.jsx(n.p,{children:"A spinner for displaying loading state of a page or a section."}),`
+`,e.jsx(r,{of:i}),`
+`,e.jsx(n.h2,{id:"playground",children:"Playground"}),`
+`,e.jsx(o,{of:i}),`
+`,e.jsx(n.h2,{id:"sizes",children:"Sizes"}),`
+`,e.jsx(n.p,{children:"Besides default size, Spinner component provides three additional sizes for you to choose among different scenarios."}),`
+`,e.jsxs(n.p,{children:["Use property ",e.jsx(n.code,{children:"size"})," to set additional sizes with ",e.jsx(n.code,{children:"large"}),", ",e.jsx(n.code,{children:"small"}),"."]}),`
+`,e.jsx(r,{of:l}),`
+`,e.jsx(n.h2,{id:"variants",children:"Variants"}),`
+`,e.jsxs(n.p,{children:["Use property ",e.jsx(n.code,{children:"variant"})," to set specific variant with ",e.jsx(n.code,{children:"primary"}),", ",e.jsx(n.code,{children:"info"}),", ",e.jsx(n.code,{children:"success"}),", ",e.jsx(n.code,{children:"warning"}),", ",e.jsx(n.code,{children:"danger"}),"."]}),`
+`,e.jsx(r,{of:a}),`
+`,e.jsx(n.hr,{}),`
+`,e.jsx(n.h2,{id:"spinner-api",children:"Spinner API"}),`
+`,e.jsxs("div",{style:{marginTop:"30px"},children:[e.jsx(n.h3,{id:"attributes",children:"Attributes"}),e.jsxs(n.table,{children:[e.jsx(n.thead,{children:e.jsxs(n.tr,{children:[e.jsx(n.th,{children:"Name"}),e.jsx(n.th,{children:"Description"}),e.jsx(n.th,{children:"Type"}),e.jsx(n.th,{children:"Default"})]})}),e.jsxs(n.tbody,{children:[e.jsxs(n.tr,{children:[e.jsx(n.td,{children:"size"}),e.jsx(n.td,{children:"spinner size"}),e.jsxs(n.td,{children:[e.jsx(n.code,{children:"large"})," | ",e.jsx(n.code,{children:"default"})," | ",e.jsx(n.code,{children:"small"})]}),e.jsx(n.td,{children:e.jsx(n.code,{children:"default"})})]}),e.jsxs(n.tr,{children:[e.jsx(n.td,{children:"variant"}),e.jsx(n.td,{children:"spinner variant"}),e.jsxs(n.td,{children:[e.jsx(n.code,{children:"primary"})," | ",e.jsx(n.code,{children:"default"})," | ",e.jsx(n.code,{children:"info"})," | ",e.jsx(n.code,{children:"success"})," | ",e.jsx(n.code,{children:"warning"})," | ",e.jsx(n.code,{children:"danger"})]}),e.jsx(n.td,{children:e.jsx(n.code,{children:"default"})})]}),e.jsxs(n.tr,{children:[e.jsx(n.td,{children:"bg"}),e.jsx(n.td,{children:"determine whether the spinner background color is always on"}),e.jsx(n.td,{children:e.jsx(n.code,{children:"boolean"})}),e.jsx(n.td,{children:"true"})]})]})]})]})]})}function S(s={}){const{wrapper:n}={...t(),...s.components};return n?e.jsx(n,{...s,children:e.jsx(d,{...s})}):d(s)}export{S as default};
