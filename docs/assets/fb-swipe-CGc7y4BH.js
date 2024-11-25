f14a8e30f0cff46ae6827222a8a34585c1331b2f
GitHub <action@github.com>
2024-11-25 18:56:30 +0000

    Removed code after split (#323)

--- docs/assets/fb-swipe-CGc7y4BH.js
+++
@@ -1,7 +0,0 @@
-import{ae as t,af as e,ag as r}from"./index-CcXXGbBB.js";import{u as s}from"./index-Sdlmj7v7.js";import{S as a,B as m}from"./fb-swipe.stories-D5Mg5XNW.js";import"./iframe-CCH1c40q.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-pVbLjA8_.js";import"./index-DrFu-skq.js";import"./index-BvDULa1j.js";import"./vue.esm-bundler-Co5bQ3ld.js";import"./web-ui-components.es-CYzxcWcv.js";function o(n){const i={h1:"h1",h2:"h2",hr:"hr",...s(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:a}),`
-`,t.jsx(i.h1,{id:"swipe",children:"Swipe"}),`
-`,t.jsx(i.h2,{id:"basic-usage",children:"Basic Usage"}),`
-`,t.jsx(r,{withToolbar:!0,of:m}),`
-`,t.jsx(i.hr,{}),`
-`,t.jsx(i.h2,{id:"swipe-api",children:"Swipe API"}),`
-`,t.jsx("div",{style:{marginTop:"30px"}})]})}function S(n={}){const{wrapper:i}={...s(),...n.components};return i?t.jsx(i,{...n,children:t.jsx(o,{...n})}):o(n)}export{S as default};
