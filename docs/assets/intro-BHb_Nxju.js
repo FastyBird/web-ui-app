ebfaaed992781cb3a65489844472a0faa0c12bb6
GitHub <action@github.com>
2024-11-26 12:30:07 +0000

    Added missing package repository (#331)

--- docs/assets/intro-BHb_Nxju.js
+++
@@ -1,9 +0,0 @@
-import{ae as r,af as s}from"./index-CcXXGbBB.js";import{u as n}from"./index-Sdlmj7v7.js";import"./iframe-CCH1c40q.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-pVbLjA8_.js";import"./index-DrFu-skq.js";function i(e){const t={code:"code",h1:"h1",h4:"h4",p:"p",pre:"pre",...n(),...e.components};return r.jsxs(r.Fragment,{children:[r.jsx(s,{title:"About/Intro"}),`
-`,r.jsx(t.h1,{id:"fastybird-web-ui-library",children:"FastyBird Web UI library"}),`
-`,r.jsx(t.p,{children:"FastyBird Web UI library is a design system built with Storybook and Vue."}),`
-`,r.jsx(t.h4,{id:"installation",children:"Installation"}),`
-`,r.jsx(t.pre,{children:r.jsx(t.code,{children:`yarn add @fastybird/web-ui-library
-`})}),`
-`,r.jsx(t.p,{children:"Import the FastyBird UI main style sheet in your application:"}),`
-`,r.jsx(t.pre,{children:r.jsx(t.code,{children:`@use "~@fastybird/web-ui-library/web-ui-library.css";
-`})})]})}function b(e={}){const{wrapper:t}={...n(),...e.components};return t?r.jsx(t,{...e,children:r.jsx(i,{...e})}):i(e)}export{b as default};
